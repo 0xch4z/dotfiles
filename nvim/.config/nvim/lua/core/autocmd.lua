bf0bee59f66790fc33b098fa36bc3bb0b41a0ba6
Charles Kenney <charles.kenney@lifeomic.com>
2022-08-16 12:21:08 -0400

    tabsize=2 for html and liquid

--- nvim/.config/nvim/lua/core/autocmd.lua
+++ nvim/.config/nvim/lua/core/autocmd.lua
@@ -1,7 +1,8 @@
 local augroup = vim.api.nvim_create_augroup
 local autocmd = vim.api.nvim_create_autocmd
 
--- Set indention to 2 spaces
+-- All filetypes not included in the autocmd below
+-- will default to a shiftwidth and tabstop of 4.
 augroup('setIndent2', { clear = true })
 autocmd('Filetype', {
   group = 'setIndent2',
@@ -11,6 +12,9 @@ autocmd('Filetype', {
     'lua',
     'javascript',
     'typescript',
+    'html',
+    'liquid',
+    'ejs',
   },
   command = 'setlocal shiftwidth=2 tabstop=2'
 })
