ff127a7b975b7b4601669e070a23f6a14a79c34a
Charles Kenney <charles.kenney@lifeomic.com>
2022-10-26 20:16:49 -0400

    add symbols-outline config

---
+++ nvim/.config/nvim/lua/plugins/symbols-outline.lua
@@ -0,0 +1,36 @@
+local status, symbols = pcall(require, "symbols-outline")
+if (not status) then return end
+
+symbols.setup({
+  highlight_hovered_item = true,
+  show_guides = true,
+  auto_preview = false,
+  position = 'right',
+  relative_width = true,
+  width = 25,
+  auto_close = true,
+  show_numbers = false,
+  show_relative_numbers = false,
+  show_symbol_details = true,
+  preview_bg_highlight = 'Pmenu',
+  autofold_depth = nil,
+  auto_unfold_hover = true,
+  wrap = false,
+  keymaps = {
+    close = {"<Esc>", "q"},
+    goto_location = "<Cr>",
+    focus_location = "o",
+    hover_symbol = "<C-space>",
+    toggle_preview = "K",
+    rename_symbol = "r",
+    code_actions = "a",
+    fold = "h",
+    unfold = "l",
+    fold_all = "W",
+    unfold_all = "E",
+    fold_reset = "R",
+  },
+  lsp_blacklist = {},
+  symbol_blacklist = {},
+})
+
