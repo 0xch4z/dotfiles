2c70a8cc195fbce73f38b6a8847f1142e9f95415
Charlie Kenney <ckenney@equinix.com>
2023-01-12 18:28:17 -0500

    simplify orgmode config

--- nvim/.config/nvim/lua/plugins/orgmode.lua
+++ nvim/.config/nvim/lua/plugins/orgmode.lua
@@ -1,19 +1,6 @@
 require('orgmode').setup_ts_grammar()
-
-require('nvim-treesitter.configs').setup {
-  -- If TS highlights are not enabled at all, or disabled via `disable` prop,
-  -- highlighting will fallback to default Vim syntax highlighting
-  highlight = {
-    enable = true,
-    -- Required for spellcheck, some LaTex highlights and
-    -- code block highlights that do not have ts grammar
-    additional_vim_regex_highlighting = {'org'},
-  },
-  ensure_installed = {'org'}, -- Or run :TSUpdate org
-}
-
 require('orgmode').setup {
-  org_agend_files = {'~/Library/Mobile\ Documents/com\~apple\~CloudDocs/notes/misc/**/*'}
+  org_agend_files = {'~/notes/misc/**/*'}
   notifications = {
     enabled = true,
     cron_enabled = true,
