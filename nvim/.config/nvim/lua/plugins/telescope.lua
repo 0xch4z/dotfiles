d83a8a62db59b1b704a05f79f24ab8c907cb5153
Charlie Kenney <me@ch4z.io>
2023-02-22 22:04:54 -0500

    update telescope config

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -1,6 +1,7 @@
 local telescope = require('telescope')
 local actions = require('telescope.actions')
 local previewers = require('telescope.previewers')
+local fb_actions = require('telescope').extensions.file_browser.actions
 
 telescope.setup({
   defaults = {
@@ -14,10 +15,30 @@ telescope.setup({
 
 		mappings = {
 			i = {
+        ["q"] = actions.close,
 				["<C-x>"] = false,
 				["<C-q>"] = actions.send_to_qflist,
         ["<CR>"] = actions.select_default,
 			},
 		},
   },
-}) ]]
+
+  extensions = {
+    file_browser = {
+      theme = "dropdown",
+      hijack_netrw = true,
+      mappings = {
+        ["n"] = {
+          -- your custom normal mode mappings
+          ["N"] = fb_actions.create,
+          ["h"] = fb_actions.goto_parent_dir,
+          ["/"] = function()
+            vim.cmd('startinsert')
+          end
+        }
+      },
+    },
+  },
+})
+
+telescope.load_extension 'file_browser'
