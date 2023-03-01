ddc646ac05cbb7d5e63bee6130f85af06e93c508
Charlie Kenney <me@ch4z.io>
2023-03-01 09:31:23 -0500

    add some dir aliases

--- zsh/zsh/aliases.zsh
+++ zsh/zsh/aliases.zsh
@@ -4,6 +4,9 @@ alias -g nerdctl=nerdctl.lima
 alias -g nerd=nerdctl
 alias -g pinentry=pinentry-mac
 alias -g watch=hwatch
+alias -g n=neovim
 
 alias -g gh="cd $HOME"
 alias -g gw="cd $HOME/work"
+alias -g gd="cd $HOME/.dotfiles"
+alias -g gn="cd $HOME/.dotfiles/nvim/.config/nvim"
