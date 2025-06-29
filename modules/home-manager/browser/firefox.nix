6174807312af489d7d879efd3f9f318db0d4deb3
Charlie Kenney <me@ch4z.io>
2025-06-28 21:36:00 -0400

    browser/firefox: add devPixelsPerPx option
    Defaults to 1.5 for mac (scaling issues) and 1.0 for linux

--- modules/home-manager/browser/firefox.nix
+++ modules/home-manager/browser/firefox.nix
@@ -1,21 +1,25 @@
 args@{ config, self, pkgs, ... }:
 let
-  inherit (self.lib) isDarwin mkIf mkDesktopEnabledOption;
+  inherit (pkgs) stdenv types;
+  inherit (stdenv.hostPlatform) isDarwin;
+  inherit (self.lib) mkIf mkDesktopEnabledOption mkOption;
 
   cfg = config.x.home.browser.firefox;
 in {
   options.x.home.browser.firefox = {
     enable = mkDesktopEnabledOption config "Enable Firefox home-manager module.";
+
+    devPixelsPerPx = mkOption {
+      type = types.string;
+      default = if isDarwin then "1.5" else "1.0"; # scaling
+    };
   };
 
   config = mkIf cfg.enable {
       programs.firefox = {
         enable = true;
 
-        package = if (isDarwin pkgs.system) then
-          pkgs.firefox-bin
-        else
-          pkgs.firefox;
+        package = if isDarwin then pkgs.firefox-bin else pkgs.firefox;
 
         policies = import ./firefox_policies.nix;
         profiles = import ./firefox_profile.nix args;
--- modules/home-manager/browser/firefox_profile.nix
+++ modules/home-manager/browser/firefox_profile.nix
@@ -177,7 +177,7 @@ in {
 
      "gfx.webrender.all" = true;
      "gfx.webrender.force-disabled" = false;
-     "layout.css.devPixelsPerPx" = "1.5";
+     "layout.css.devPixelsPerPx" = config.x.home.browser.firefox.devPixelsPerPx;
 
      "browser.uidensity" = 1;
      "browser.tabs.drawInTitlebar" = true;
