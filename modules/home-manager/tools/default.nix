4c44392c44df54e1baa8be023a82ff4033c3dc0e
Charlie Kenney <me@ch4z.io>
2025-08-05 16:24:18 -0400

    tools: add coreutils essential/full

--- modules/home-manager/tools/default.nix
+++ modules/home-manager/tools/default.nix
@@ -1,4 +1,16 @@
-{
+{ self, config, pkgs, ... }:
+let
+  inherit (self.lib) mkIf mkOption mkEnableOption;
+
+  cfg = config.x.home.tools;
+in {
+  options.x.home.tools = {
+    coreutils = mkOption {
+      type = self.lib.types.enum ["essential" "full" "none"];
+      default = "none";
+    };
+  };
+
   imports = [
     ./containers
     ./infrastructure
@@ -7,4 +19,11 @@
     ./productivity.nix
     ./secrets.nix
   ];
+
+  config = {
+    home.packages = with pkgs; [
+      (mkIf (cfg.coreutils == "essential") coreutils)
+      (mkIf (cfg.coreutils == "full") coreutils-full)
+    ];
+  };
 }
