f51ab6bcb0db437551af9b6e64389f647564eb45
Charlie Kenney <charles.kenney@isovalent.com>
2026-03-10 12:12:28 -0400

    development/build: add system gcc + clang dependency

--- modules/home-manager/development/build.nix
+++ modules/home-manager/development/build.nix
@@ -1,15 +1,40 @@
-{ pkgs, config, lib, ... }: {
+{
+  pkgs,
+  config,
+  lib,
+  ...
+}:
+{
   config = lib.mkIf config.x.home.development.enable {
-    home.packages = with pkgs; [
-      actionlint
-      buildpack
-      cmake
-      envsubst
-      gnumake
-      go-task
-      graphviz
-      plantuml
-      gemini-cli
-    ];
+    home.packages =
+      with pkgs;
+      [
+        actionlint
+        buildpack
+        clang
+        (lib.hiPrio gcc)
+        cmake
+        envsubst
+        gnumake
+        go-task
+        graphviz
+        openssl.dev
+        pkg-config
+        plantuml
+        gemini-cli
+      ]
+      ++ lib.optionals stdenv.hostPlatform.isLinux [
+        dbus.dev
+      ];
+
+    home.sessionVariables = {
+      PKG_CONFIG_PATH =
+        lib.makeSearchPath "lib/pkgconfig" [
+          pkgs.openssl.dev
+        ]
+        + lib.optionalString pkgs.stdenv.hostPlatform.isLinux ":${
+          lib.makeSearchPath "lib/pkgconfig" [ pkgs.dbus.dev ]
+        }";
+    };
   };
 }
