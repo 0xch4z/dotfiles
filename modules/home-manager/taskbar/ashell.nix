7960e285aba681ba67cb230999323953e846f7b1
Charlie Kenney <me@ch4z.io>
2026-06-14 17:20:23 -0400

    taskbar/ashell: add ashell systemd service

--- modules/home-manager/taskbar/ashell.nix
+++ modules/home-manager/taskbar/ashell.nix
@@ -1,6 +1,13 @@
-{ config, lib, pkgs, ... }:
-let cfg = config.x.home.taskbar.ashell;
-in {
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  cfg = config.x.home.taskbar.ashell;
+in
+{
   options.x.home.taskbar.ashell = {
     enable = lib.mkEnableOption "Enable ashell home-manager module.";
   };
@@ -8,16 +15,23 @@ in {
   config = lib.mkIf cfg.enable {
     programs.ashell = {
       enable = true;
+      systemd.enable = true;
       settings = {
         position = "Bottom";
 
         modules = {
-          left = [ "Workspaces" "SystemInfo" ];
+          left = [
+            "Workspaces"
+            "SystemInfo"
+          ];
           center = [ "WindowTitle" ];
           right = [ "Clock" ];
         };
 
-        system_info.indicators = [ "Cpu" "Memory" ];
+        system_info.indicators = [
+          "Cpu"
+          "Memory"
+        ];
 
         clock.format = "%a %d %b  %H:%M";
 
@@ -25,6 +39,14 @@ in {
           style = "Islands";
           opacity = 0.9;
         };
+      };
+    };
+
+    systemd.user.services.ashell = {
+      Unit.StartLimitIntervalSec = 0;
+      Service = {
+        Restart = lib.mkForce "always";
+        RestartSec = 1;
       };
     };
   };
