83c4b5d9265c9e3f8012f1629d08415f73a81c64
Charlie Kenney <me@ch4z.io>
2026-06-14 17:26:52 -0400

    development: add direnv + devenv

--- modules/home-manager/development/shell.nix
+++ modules/home-manager/development/shell.nix
@@ -1,9 +1,22 @@
-{pkgs, config, lib, ...}: {
+{
+  pkgs,
+  config,
+  lib,
+  ...
+}:
+{
   config = lib.mkIf config.x.home.development.enable {
-    home.packages = with pkgs; [
-      bashInteractive
-      shellcheck
-      shfmt
-    ];
+    home.packages =
+      with pkgs;
+      [
+        bashInteractive
+        devenv
+        direnv
+        shellcheck
+        shfmt
+      ]
+      ++ lib.optionals pkgs.stdenv.hostPlatform.isLinux [
+        bubblewrap
+      ];
   };
 }
