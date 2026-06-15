a9c2077bfa276fa31339d300f9a3074fc9683cbb
Charlie Kenney <me@ch4z.io>
2026-06-14 21:47:00 -0400

    machines/charbox2wsl: use pkgs.mesa for nvidia driver

--- machines/charbox2wsl/default.nix
+++ machines/charbox2wsl/default.nix
@@ -40,7 +40,7 @@
   };
 
   hardware.graphics.enable = true;
-  hardware.graphics.extraPackages = [ pkgs.mesa.drivers ];
+  hardware.graphics.extraPackages = [ pkgs.mesa ];
 
   nix = {
     settings = {
